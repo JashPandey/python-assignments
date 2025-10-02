ba83983fae219d613a30aa9f5b063001268b94b9
JashPandey <jashpandeyofficial@gmail.com>
2025-10-02 21:15:24 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+a=input("enter the first number : ") #takes an input and stores it in a 
+b=input("enter the second number: ") #takes an input and stores it in b
+
+c=(a+b) 
+d=(b-a)
+e=(a*b)
+f=(a/b)
+print("Addition :" ,c)
+print("subtraction: ",d)
+print("multiplication : ",e)
+print("divison : ",f)
