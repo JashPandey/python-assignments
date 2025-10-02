ab2826c395a68c2587e10f3c3388f42895c09d79
JashPandey <jashpandeyofficial@gmail.com>
2025-10-02 21:54:04 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,4 @@
+first_name=input("please enter your first name :")
+last_name=inpkut("please enter your last name : ")
+full_name= first_name + "  " + last_name
+print("hello!" + full_name + "welcome to the pyhton program")
