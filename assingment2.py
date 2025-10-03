c23caa0d8dd9e726913220ba5060e2f8f08f5396
JashPandey <jashpandeyofficial@gmail.com>
2025-10-03 21:35:01 +0530

    Add files via upload

---
+++ assingment2.py
@@ -0,0 +1,11 @@
+#task 1
+a=int(input("please enter a number: "))
+if (a%2==0):
+    print("the given number is even")
+else:print("the number is odd")
+
+#task 2
+total_sum=0
+for i in range (1,51):
+    total_sum=total_sum+i
+print("the sum of numbers from 1 to 50 is ",total_sum)
