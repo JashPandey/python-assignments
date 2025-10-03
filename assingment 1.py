e397658b15f4196e45075ba1d7fee9f50f295410
JashPandey <jashpandeyofficial@gmail.com>
2025-10-03 19:34:48 +0530

    Add files via upload

---
+++ assingment 1.py
@@ -0,0 +1,17 @@
+# task 1
+a=int(input("enter the first number : "))
+b=int(input("enter the second number : "))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("addition =",c)
+print("subtraction =",d)
+print("multiplication =",e)
+print("division =",f)
+# task 2
+first_name=input("enter the first name : ")
+last_name=input("enter the last name : ")
+full_name=first_name+" "+last_name
+print("hello!"+ full_name + "  " + "welcome to the python program" )
+
